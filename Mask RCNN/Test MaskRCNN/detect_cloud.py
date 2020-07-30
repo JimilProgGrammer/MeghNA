83e1d7b2176f1ab2f9b5c4ea05e64edfe861c655
shlokashah <shlokashah0603@gmail.com>
2020-07-30 15:03:48 +0530

    updates in mask rcnn

--- Mask RCNN/Test MaskRCNN/detect_cloud.py
+++ Mask RCNN/Test MaskRCNN/detect_cloud.py
@@ -5,7 +5,7 @@ import os
 DOWNLOAD H5 FILE FROM DRIVE AND PLACE IN THE SAME FOLDER. mask_rcnn_cloud_0010.h5
 '''
 def main():
-	os.system("python mask_rcnn_predict.py --weights mask_rcnn_cloud_0010.h5 --labels labels.txt --image images/satellite13.jpg ")
+	os.system("python mask_rcnn_predict.py --weights tir.h5 --labels labels.txt --image images/satellite45.jpg ")
 
 if __name__ == '__main__':
 	main()
--- Mask RCNN/Test MaskRCNN/mask_rcnn_predict.py
+++ Mask RCNN/Test MaskRCNN/mask_rcnn_predict.py
@@ -11,7 +11,6 @@ import os
 import matplotlib.pyplot as plt
 from collections import defaultdict
 
-
 ap = argparse.ArgumentParser()
 ap.add_argument("-w", "--weights", required=True,
 	help="path to Mask R-CNN model weights")
@@ -20,7 +19,6 @@ ap.add_argument("-l", "--labels", required=True,
 ap.add_argument("-i", "--image", required=True,
 	help="path to input image to apply Mask R-CNN to")
 args = vars(ap.parse_args())
-
 
 CLASS_NAMES = open(args["labels"]).read().strip().split("\n")
 
@@ -44,24 +42,29 @@ model.load_weights(args["weights"], by_name=True)
 
 
 image = cv2.imread(args["image"])
+# img = cv2.imread(args["image"],0)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 image = imutils.resize(image, width=512)
-
 
 print("[INFO] making predictions with Mask R-CNN...")
 r = model.detect([image], verbose=1)[0]
 
-
+cloud_coordinates = {}
 for i in range(0, r["rois"].shape[0]):
 	classID = r["class_ids"][i]
 	mask = r["masks"][:, :, i]
 	color = COLORS[classID][::-1]
+	coordinates = np.where(mask == True)
+	x = coordinates[1]
+	y = coordinates[0]
+	cloud_coordinates[str(i)] = {'x':x,'y':y}
 	image = visualize.apply_mask(image, mask, color, alpha=0.5)
-
+	plt.imshow(image, cmap = 'gray', interpolation = 'bicubic')
+	print(cloud_coordinates)
 
 image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-
+count = 0
 for i in range(0, len(r["scores"])):
 	(startY, startX, endY, endX) = r["rois"][i]
 	classID = r["class_ids"][i]
@@ -69,10 +72,10 @@ for i in range(0, len(r["scores"])):
 	score = r["scores"][i]
 	color = [int(c) for c in np.array(COLORS[classID]) * 255]
 	cv2.rectangle(image, (startX, startY), (endX, endY), color, 2)
-	text = "{}: {:.3f}".format(label, score)
+	text = "{}:{:}: {:.3f}".format(label,count, score)
 	y = startY - 10 if startY - 10 > 10 else startY + 10
-	cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX,
-		0.6, color, 2)
+	cv2.putText(image, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX,0.6, color,2)
+	count = count + 1
 
 com = defaultdict(list)
 for i in range(r['rois'].shape[0]):
@@ -81,11 +84,11 @@ for i in range(r['rois'].shape[0]):
 	y = (l[0]+l[2])/2
 	com[i].append(x)
 	com[i].append(y)
-print(r['rois'])
+# print(r['rois'])
 print(r['masks'])
 print(com)
-
-
+with open("com.txt","a") as f:
+	print(com,file=f)
 plt.imshow(image, cmap = 'gray', interpolation = 'bicubic')
 plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
 plt.show()
